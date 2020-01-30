e2402057723ef967c9a6ce066b6c9a3bf99dedd2
mantasoff <mantas.paskevicius1996@gmail.com>
2020-01-30 19:06:00 +0200

    Initial Commit

---
+++ iOSPingerApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSPingerApp
+//
+//  Created by Mantas Paškevičius on 30/01/2020.
+//  Copyright © 2020 Mantas Paškevičius. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
